2a9108a76f49133b54eb11a0e6cf51471a4cdbe3
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2020-10-23 17:01:02 -0600

    edit other.sh

--- other.sh
+++ other.sh
@@ -5,7 +5,7 @@ cd yay
 makepkg -si
 cd ..
 
-yay -S --needed otf-nerd-fonts-fira-code protonvpn-cli-ng zork1 zork2 zork3
+yay -Syu --needed otf-nerd-fonts-fira-code protonvpn-cli-ng zork1 zork2 zork3
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
@@ -13,6 +13,13 @@ source $HOME/.cargo/env
 
 cargo install cargo-update
 cargo install starship
+
+source ~/.zshrc
+source ~/.zprofile
+mkdir ~/.zfunc
+rustup completions zsh > ~/.zfunc/_rustup
+rustup completions zsh cargo > ~/.zfunc/_cargo
+exec zsh
 
 curl -sLf https://spacevim.org/install.sh | bash
 nvim
