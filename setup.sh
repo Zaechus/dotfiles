9239e2240970fb20e871d3990534457567367524
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2020-10-23 23:46:13 -0600

    use xinitrc in setup

--- setup.sh
+++ setup.sh
@@ -18,3 +18,11 @@ sudo useradd -m xamax && sudo passwd xamax
 sudo usermod -a -G video lewxam
 sudo usermod -a -G games lewxam
 sudo usermod -a -G video xamax
+
+sudo cp /etc/X11/xinit/xinitrc /home/lewxam/.xinitrc
+sudo chown lewxam:lewxam /home/lewxam/.xinitrc
+sudo nvim /home/lewxam/.xinitrc
+
+sudo cp /etc/X11/xinit/xinitrc /home/xamax/.xinitrc
+sudo chown xamax:xamax /home/xamax/.xinitrc
+sudo nvim /home/xamax/.xinitrc
