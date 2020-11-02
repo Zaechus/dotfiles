e71398c88d85ccaaaa3d7c2ad4f8e381d3ac3651
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2020-11-01 19:22:10 -0700

    separate hw specific drivers into diff scripts

---
+++ amd.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo pacman -S amdvlk lib32-amdvlk vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver xf86-video-amdgpu xf86-video-ati
---
+++ intel.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo pacman -S vulkan-intel libva-intel-driver lib32-vulkan-intel lib32-libva-intel-driver
