b03677f306a6bf5899063bf1eec5cb5fdfd16b8f
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2020-12-26 21:46:02 -0700

    discord added

--- games.sh
+++ games.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo pacman -S --needed angband dosbox gnuchess gnugo libretro-genesis-plus-gx libretro-mgba libretro-mupen64plus-next libretro-nestopia libretro-parallel-n64 libretro-snes9x retroarch rogue tk xorg-server xorg-xinit i3-gaps dmenu
+sudo pacman -S --needed angband discord dosbox gnuchess gnugo libretro-genesis-plus-gx libretro-mgba libretro-mupen64plus-next libretro-nestopia libretro-parallel-n64 libretro-snes9x retroarch rogue tk xorg-server xorg-xinit i3-gaps dmenu
 
 cargo install gameoflife
