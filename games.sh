6368e058544328a8095ad59d76bf5deff6125379
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-03-08 12:40:54 -0700

    replace i3 with lxqt

--- games.sh
+++ games.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-sudo pacman -S --needed angband discord dosbox gnuchess gnugo libretro-genesis-plus-gx libretro-mgba libretro-mupen64plus-next libretro-nestopia libretro-parallel-n64 libretro-snes9x retroarch rogue tk xorg-server xorg-xinit i3-gaps dmenu
+sudo pacman -S --needed angband discord dosbox gnuchess gnugo libretro-genesis-plus-gx libretro-mgba libretro-mupen64plus-next libretro-nestopia libretro-parallel-n64 libretro-snes9x retroarch rogue tk xorg-server xorg-xinit lxqt
 
+sudo pacman -Rns qterminal lxqt-about
+
+paru -S minecraft-launcher
+
+cargo install kingslayer
 cargo install gameoflife
