09ed80ef89c902a6a466c3b9fc580cbea0b0cccf
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-03-23 13:32:53 -0600

    allow write access to /var/games for users in games group

--- setup.sh
+++ setup.sh
@@ -23,6 +23,8 @@ sudo usermod -a -G lp lewxam
 sudo usermod -a -G audio lewxam
 sudo usermod -a -G video xamax
 
+sudo chmod 775 /var/games
+
 sudo cp /etc/X11/xinit/xinitrc /home/lewxam/.xinitrc
 sudo chown lewxam:lewxam /home/lewxam/.xinitrc
 sudo nvim /home/lewxam/.xinitrc
