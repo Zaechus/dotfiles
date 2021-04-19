77caa7b8df30d38563b07e6fb31ae449a48924d4
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-04-19 17:39:49 -0600

    remove lxqt-archiver

--- games.sh
+++ games.sh
@@ -2,7 +2,7 @@
 
 sudo pacman -S --needed angband discord dosbox gnuchess gnugo libretro-genesis-plus-gx libretro-mgba libretro-mupen64plus-next libretro-nestopia libretro-parallel-n64 libretro-snes9x retroarch rogue tk xorg-server xorg-xinit lxqt
 
-sudo pacman -Rns qterminal lxqt-about
+sudo pacman -Rns qterminal lxqt-about lxqt-archiver
 
 paru -S minecraft-launcher
 
