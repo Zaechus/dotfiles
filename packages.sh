55266ffe6db3ca84a8a65378a186467c527f0f18
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-04-26 13:00:18 -0600

    temp remove ovmf

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo pacman -Syyuu --needed alacritty amd-ucode artix-background backlight-openrc bat bemenu brightnessctl clang cmake dotnet-runtime dotnet-sdk evince exa fd firefox-developer-edition git go go-tools imv links mpv neofetch neovim noto-fonts noto-fonts-emoji npm nuget openrc-zsh-completions otf-fira-mono otf-fira-sans otf-font-awesome pacman pulseaudio pulseaudio-alsa python python-pip python-pynvim ripgrep stow sway swaylock grim texlive-core tmux tokei ttf-liberation typescript unzip waybar wl-clipboard woff2-fira-code zsh-autosuggestions zsh-completions zsh-syntax-highlighting pulseaudio-alsa qemu ovmf aria2 cowsay lld yasm wget
+sudo pacman -Syyuu --needed alacritty amd-ucode artix-background backlight-openrc bat bemenu brightnessctl clang cmake dotnet-runtime dotnet-sdk evince exa fd firefox-developer-edition git go go-tools imv links mpv neofetch neovim noto-fonts noto-fonts-emoji npm nuget openrc-zsh-completions otf-fira-mono otf-fira-sans otf-font-awesome pacman pulseaudio pulseaudio-alsa python python-pip python-pynvim ripgrep stow sway swaylock grim texlive-core tmux tokei ttf-liberation typescript unzip waybar wl-clipboard woff2-fira-code zsh-autosuggestions zsh-completions zsh-syntax-highlighting pulseaudio-alsa qemu aria2 cowsay lld yasm wget
 
 sudo rc-update add backlight
 
