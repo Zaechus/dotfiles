8f66684fcc43ecb878c0dd7205f025817f321b6e
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-05-06 21:49:58 -0600

    change utils service to persistenced

--- graphics/nvidia.sh
+++ graphics/nvidia.sh
@@ -2,4 +2,4 @@
 
 sudo pacman -S nvidia nvidia-utils lib32-nvidia-utils nvidia-settings nvidia-utils-openrc libva-vdpau-driver libva-utils vdpauinfo
 
-sudo rc-update add nvidia-utils
+sudo rc-update add nvidia-persistenced
