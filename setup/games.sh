9e9b5d55dbc3310ad8d8488bddd98fa91a419b31
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-05-06 21:28:13 -0600

    move j4-dmenu-desktop to paru command

--- setup/games.sh
+++ setup/games.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-sudo pacman -S --needed angband discord dosbox gnuchess gnugo libretro-genesis-plus-gx libretro-mgba libretro-mupen64plus-next libretro-nestopia libretro-parallel-n64 libretro-snes9x retroarch rogue xorg-server xorg-xinit i3-gaps j4-dmenu-desktop pcmanfm-qt
+sudo pacman -S --needed angband discord dosbox gnuchess gnugo libretro-genesis-plus-gx libretro-mgba libretro-mupen64plus-next libretro-nestopia libretro-parallel-n64 libretro-snes9x retroarch rogue xorg-server xorg-xinit i3-gaps pcmanfm-qt
 
-paru -S minecraft-launcher
+paru -S minecraft-launcher j4-dmenu-desktop
 
 cargo install kingslayer
 cargo install gameoflife
