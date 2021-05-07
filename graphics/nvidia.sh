06f6ef1f7b4d110e9cfdf7bc1551e02019989e23
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-05-06 20:44:50 -0600

    add nvtop to trusted

--- graphics/nvidia.sh
+++ graphics/nvidia.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo pacman -S nvidia nvidia-utils lib32-nvidia-utils nvidia-settings nvidia-utils-openrc libva-vdpau-driver libva-utils vdpauinfo lib32-libva-vdpau-driver
+sudo pacman -S nvidia nvidia-utils lib32-nvidia-utils nvidia-settings nvidia-utils-openrc libva-vdpau-driver libva-utils vdpauinfo lib32-libva-vdpau-driver nvtop
 
 sudo rc-update add nvidia-persistenced
 
