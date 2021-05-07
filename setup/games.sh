a09b25e277f6d0bcb6d87f3d47fe0fe7297eb3fd
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-05-06 23:10:25 -0600

    j4 to rofi

--- setup/games.sh
+++ setup/games.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-sudo pacman -S --needed angband discord dosbox gnuchess gnugo libretro-genesis-plus-gx libretro-mgba libretro-mupen64plus-next libretro-nestopia libretro-parallel-n64 libretro-snes9x retroarch rogue xorg-server xorg-xinit i3-gaps pcmanfm-qt
+sudo pacman -S --needed rofi angband discord dosbox gnuchess gnugo libretro-genesis-plus-gx libretro-mgba libretro-mupen64plus-next libretro-nestopia libretro-parallel-n64 libretro-snes9x retroarch rogue xorg-server xorg-xinit i3-gaps pcmanfm-qt
 
-paru -S minecraft-launcher j4-dmenu-desktop
+paru -S minecraft-launcher
 
 cargo install kingslayer
 cargo install gameoflife
