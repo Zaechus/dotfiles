25b126271c8ef9b97e8c4f0e5b7e3f2fb2a5c508
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-05-07 07:17:03 -0600

    slock to i3lock

--- setup/games.sh
+++ setup/games.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo pacman -S --needed rofi angband discord dosbox gnuchess gnugo libretro-genesis-plus-gx libretro-mgba libretro-mupen64plus-next libretro-nestopia libretro-parallel-n64 libretro-snes9x retroarch rogue xorg-server xorg-xinit i3-gaps pcmanfm-qt slock
+sudo pacman -S --needed rofi angband discord dosbox gnuchess gnugo libretro-genesis-plus-gx libretro-mgba libretro-mupen64plus-next libretro-nestopia libretro-parallel-n64 libretro-snes9x retroarch rogue xorg-server xorg-xinit i3-gaps pcmanfm-qt i3lock
 
 paru -S minecraft-launcher
 
