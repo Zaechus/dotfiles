58b88d17001dfe309ca8416e2e1fbb1d27860764
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-05-06 22:01:21 -0600

    add lib32 libva vdpau driver

--- graphics/nvidia.sh
+++ graphics/nvidia.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo pacman -S nvidia nvidia-utils lib32-nvidia-utils nvidia-settings nvidia-utils-openrc libva-vdpau-driver libva-utils vdpauinfo
+sudo pacman -S nvidia nvidia-utils lib32-nvidia-utils nvidia-settings nvidia-utils-openrc libva-vdpau-driver libva-utils vdpauinfo lib32-libva-vdpau-driver
 
 sudo rc-update add nvidia-persistenced
