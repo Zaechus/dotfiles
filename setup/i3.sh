da7ef265f699b401ee46ddc2a74b6514a63b51ae
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-05-10 22:38:17 -0600

    use mod shift d for prime run menu

--- setup/i3.sh
+++ setup/i3.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-printf '\n#bindsym $mod+Shift+s exec i3lock -c 222222\n#bindcode $mod+40 exec "prime-run rofi -modi drun,run -show drun"\n#bindsym $mod+Shift+g exec "lutris"\n#bar {\n\t#position top\n#}\n' >> ~/.config/i3/config
+printf '\n#bindsym $mod+Shift+s exec i3lock -c 222222\n#bindcode $mod+40 exec "rofi -modi drun,run -show drun"\n#bindsym $mod+Shift+d exec "prime-run rofi -modi drun,run -show drun"\n#bindsym $mod+Shift+g exec "lutris"\n#bar {\n\t#position top\n#}\n' >> ~/.config/i3/config
