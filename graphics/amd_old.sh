f82e81ff1cc90a373953c78b343e27432d931075
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-05-11 21:56:28 -0600

    finish amd_old sh

--- graphics/amd_old.sh
+++ graphics/amd_old.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-sudo pacman -S xf86-video-ati mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon amdvlk lib32-amdvlk vulkan-icd-loader lib32-vulkan-icd-loader
+sudo pacman -S xf86-video-ati mesa lib32-mesa amdvlk lib32-amdvlk vulkan-icd-loader lib32-vulkan-icd-loader
+
+sudo bash -c "echo \"blacklist radeon\" > /etc/modprobe.d/blacklist.conf"
+
+sudo bash -c "echo \"#radeon.si_support=0 radeon.cik_support=0 amdgpu.si_support=1 amdgpu.cik_support=1 radeon.dpm=0\" >> /etc/default/grub"
+sudo nvim /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
