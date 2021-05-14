b76a0578eb347b2cb110b4f45f567a6060bcf714
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-05-13 21:04:33 -0600

    add sudo to update grub

--- update_grub.sh
+++ update_grub.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --removable --recheck
-grub-mkconfig -o /boot/grub/grub.cfg
+sudo grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --removable --recheck
+sudo grub-mkconfig -o /boot/grub/grub.cfg
