e7a6e0de83a59e4ca1a88ed8b3ae256b73e1c13d
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-05-20 13:54:35 -0600

    Revert "potential angband solution"
    This reverts commit ecdd71dcc2f7fbfbee4fb943c8152587af1b0abe.

--- stowall.sh
+++ stowall.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+<<<<<<< HEAD
 sudo chgrp -R games angband
 sudo chmod -R 775 angband
 mkdir /var/games/angband
@@ -7,5 +8,10 @@ stow -t /var/games/angband angband
 sudo chgrp -R games /var/games/angband
 sudo chmod -R 775 /var/games/angband
 stow -d /var/games/ -t $HOME angband
+=======
+stow -t /var/games angband
+sudo chown -R root:games /var/games/.angband
+sudo chmod -R 775 /var/games/.angband
+>>>>>>> parent of ecdd71d (potential angband solution)
 
 stow alacritty git vim wayland zsh zsh-main
