cc1109a586b0d2d8d24b911304889edfd61b550f
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-05-20 13:59:11 -0600

    fix mistakes

--- stowall.sh
+++ stowall.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
-<<<<<<< HEAD
 sudo chgrp -R games angband
 sudo chmod -R 775 angband
-mkdir /var/games/angband
-stow -t /var/games/angband angband
+cp -r angband /var/games/
 sudo chgrp -R games /var/games/angband
 sudo chmod -R 775 /var/games/angband
+
 stow -d /var/games/ -t $HOME angband
-=======
-stow -t /var/games angband
-sudo chown -R root:games /var/games/.angband
-sudo chmod -R 775 /var/games/.angband
->>>>>>> parent of ecdd71d (potential angband solution)
 
 stow alacritty git vim wayland zsh zsh-main
