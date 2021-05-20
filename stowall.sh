b98158a0c8ba66faa4ed9c5842c3f2510f84c9fa
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-05-20 12:57:16 -0600

    use angband in /var/games

--- stowall.sh
+++ stowall.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-stow alacritty angband git vim wayland zsh zsh-main
+stow -t /var/games angband
+sudo chown -R root:games /var/games/.angband
+sudo chmod -R 775 /var/games/.angband
+
+stow alacritty git vim wayland zsh zsh-main
