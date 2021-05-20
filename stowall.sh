ecdd71dcc2f7fbfbee4fb943c8152587af1b0abe
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-05-20 13:42:27 -0600

    potential angband solution

--- stowall.sh
+++ stowall.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-stow -t /var/games angband
-sudo chown -R root:games /var/games/.angband
-sudo chmod -R 775 /var/games/.angband
+sudo chgrp -R games angband
+sudo chmod -R 775 angband
+cp -r angband /var/games/
+sudo chgrp -R games /var/games/angband
+sudo chmod -R 775 /var/games/angband
+
+stow -d /var/games/ -t $HOME angband
 
 stow alacritty git vim wayland zsh zsh-main
