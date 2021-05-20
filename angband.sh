7af9b05d0292236a0b6e13c437e84e7914f0f922
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-05-20 14:08:31 -0600

    update angband.sh

--- angband.sh
+++ angband.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-sudo chgrp -R games angband
-sudo chmod -R 775 angband
 sudo chgrp -R games /var/games/angband
 sudo chmod -R 775 /var/games/angband
+cp -r /var/games/angband ./
+sudo chgrp -R games angband
+sudo chmod -R 775 angband
