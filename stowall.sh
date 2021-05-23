0af4ac0442cede93902b61dbf6fc43c9e385fa54
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-05-23 15:51:11 -0600

    add comment to stowall

--- stowall.sh
+++ stowall.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+# rm -r $HOME/.angband/Angband/customize && stow -d ./angband/.angband/Angband/ -t $HOME/.angband/Angband customize
+
 stow angband alacritty git vim wayland zsh zsh-main
