567a48298eae4b5062b7e96ed53bf3a57a229dec
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-05-23 15:42:17 -0600

    revert back to old angband ways

--- angband.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo chgrp -R games /var/games/angband
-sudo chmod -R 775 /var/games/angband
-sudo cp -r /var/games/angband ./
-sudo chgrp -R games angband
-sudo chmod -R 775 angband
--- stowall.sh
+++ stowall.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 
-sudo chgrp -R games angband
-sudo chmod -R 775 angband
-cp -r angband /var/games/
-sudo chgrp -R games /var/games/angband
-sudo chmod -R 775 /var/games/angband
-
-stow -d /var/games/ -t $HOME angband
-
-stow alacritty git vim wayland zsh zsh-main
+stow angband alacritty git vim wayland zsh zsh-main
