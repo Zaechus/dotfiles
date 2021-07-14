e2132828fe64946f3d57362148d4bfc48682b775
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-07-14 03:42:24 -0600

    rankmirrors-artix not found, remove artix suffix

--- mirrors.sh
+++ mirrors.sh
@@ -3,4 +3,4 @@
 sudo reflector --verbose --latest 64 --sort rate --save /etc/pacman.d/mirrorlist-arch
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 sudo curl https://gitea.artixlinux.org/packagesA/artix-mirrorlist/raw/branch/master/trunk/mirrorlist -o /etc/pacman.d/mirrorlist.new
-sudo su -c "rankmirrors-artix -v /etc/pacman.d/mirrorlist.new > /etc/pacman.d/mirrorlist"
+sudo su -c "rankmirrors -v /etc/pacman.d/mirrorlist.new > /etc/pacman.d/mirrorlist"
