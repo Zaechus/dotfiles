b375b9a263c30ddf8feb4ca9070974fef19bc27d
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-10-20 13:17:33 -0600

    mirrors.sh uses pacnew mirrorlist if available, otherwise curl

--- mirrors.sh
+++ mirrors.sh
@@ -2,5 +2,8 @@
 
 sudo reflector --verbose --latest 64 --sort rate --save /etc/pacman.d/mirrorlist-arch
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-sudo curl https://gitea.artixlinux.org/packagesA/artix-mirrorlist/raw/branch/master/trunk/mirrorlist -o /etc/pacman.d/mirrorlist.new
-sudo su -c "rankmirrors -v /etc/pacman.d/mirrorlist.new > /etc/pacman.d/mirrorlist"
+if [[ ! -f /etc/pacman.d/mirrorlist.pacnew ]]; then
+    sudo curl https://gitea.artixlinux.org/packagesA/artix-mirrorlist/raw/branch/master/trunk/mirrorlist -o /etc/pacman.d/mirrorlist.pacnew
+fi
+sudo su -c "rankmirrors -v /etc/pacman.d/mirrorlist.pacnew > /etc/pacman.d/mirrorlist"
+sudo mv /etc/pacman.d/mirrorlist.pacnew /etc/pacman.d/mirrorlist.new
