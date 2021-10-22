f857a057120999aaa8ac063d260ba8b54f27e604
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-10-21 22:03:48 -0600

    remove wget and reflector

--- mirrors.sh
+++ mirrors.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-sudo reflector --verbose --latest 64 --sort rate --save /etc/pacman.d/mirrorlist-arch
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 if [[ ! -f /etc/pacman.d/mirrorlist.pacnew ]]; then
     sudo curl https://gitea.artixlinux.org/packagesA/artix-mirrorlist/raw/branch/master/trunk/mirrorlist -o /etc/pacman.d/mirrorlist.pacnew
