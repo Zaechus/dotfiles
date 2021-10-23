3b635bfb43b006c0144486b68126d8511efc84f0
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-10-22 20:14:28 -0600

    remove ntp

--- setup/packages.sh
+++ setup/packages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo pacman -Syyuu --needed alacritty artix-backgrounds backlight-openrc bat bemenu bottom brightnessctl clang cmake dotnet-runtime dotnet-sdk exa fd firefox-developer-edition go go-tools imv links mpv noto-fonts noto-fonts-emoji npm nuget openrc-zsh-completions otf-fira-mono otf-fira-sans otf-font-awesome pulseaudio pulseaudio-alsa python-pip python-pynvim ripgrep stow sway swaylock grim texlive-core tmux tokei ttf-liberation typescript unzip waybar wl-clipboard woff2-fira-code zsh-autosuggestions zsh-completions zsh-syntax-highlighting pulseaudio-alsa qemu aria2 cowsay yasm edk2-ovmf man-db ntp prettier rsync pacman-contrib
+sudo pacman -Syyuu --needed alacritty artix-backgrounds backlight-openrc bat bemenu bottom brightnessctl clang cmake dotnet-runtime dotnet-sdk exa fd firefox-developer-edition go go-tools imv links mpv noto-fonts noto-fonts-emoji npm nuget openrc-zsh-completions otf-fira-mono otf-fira-sans otf-font-awesome pulseaudio pulseaudio-alsa python-pip python-pynvim ripgrep stow sway swaylock grim texlive-core tmux tokei ttf-liberation typescript unzip waybar wl-clipboard woff2-fira-code zsh-autosuggestions zsh-completions zsh-syntax-highlighting pulseaudio-alsa qemu aria2 cowsay yasm edk2-ovmf man-db prettier rsync pacman-contrib
 
 sudo rc-update add backlight
 
@@ -8,5 +8,4 @@ sudo nvim /etc/connman/main.conf
 
 sudo nvim /etc/rc.conf
 
-sudo ntpd -qg
 sudo hwclock --systohc
