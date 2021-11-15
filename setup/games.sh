b6e6e2f6e0caabad8fa1bd9e9d0c1027b812deaa
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-11-14 19:22:13 -0700

    remove additional user setup; add games and xorg-xwayland to trusted; eventually remove i3

--- setup/games.sh
+++ setup/games.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo pacman -S rofi angband discord dosbox gnuchess gnugo retroarch rogue xorg-server xorg-xinit i3-gaps pcmanfm-qt i3lock
+sudo pacman -S xorg-xwayland rofi angband discord dosbox gnuchess gnugo retroarch rogue xorg-server xorg-xinit i3-gaps pcmanfm-qt i3lock cataclysm-dda stone-soup
 
 paru -S minecraft-launcher
 
--- setup/setup.sh
+++ setup/setup.sh
@@ -11,26 +11,4 @@ cd ~
 
 sudo nvim /etc/elogind/logind.conf
 
-echo "bacchus"
-sudo useradd -m bacchus && sudo passwd bacchus
-
-echo "thor"
-sudo useradd -m thor && sudo passwd thor
-
-sudo usermod -a -G video bacchus
-sudo usermod -a -G games bacchus
-sudo usermod -a -G lp bacchus
-sudo usermod -a -G audio bacchus
-sudo usermod -a -G video thor
-
 sudo chmod 775 /var/games
-
-sudo cp /etc/X11/xinit/xinitrc /home/bacchus/.xinitrc
-sudo chown bacchus:bacchus /home/bacchus/.xinitrc
-sudo nvim /home/bacchus/.xinitrc
-sudo cp nostow/.zshrc /home/bacchus/
-sudo chown bacchus:bacchus /home/bacchus/.zshrc
-
-sudo cp /etc/X11/xinit/xinitrc /home/thor/.xinitrc
-sudo chown thor:thor /home/thor/.xinitrc
-sudo nvim /home/thor/.xinitrc
