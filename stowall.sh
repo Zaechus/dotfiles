a02d6a2580861a2b4481ac61f1ee57458c28ad53
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-11-24 23:51:37 -0700

    swap order in stowall

--- stowall.sh
+++ stowall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
+stow alacritty git vim wayland xorg zsh
+
 cp /usr/share/angband/angband/customize/* ~/.angband/Angband/customize/
 cp ~/.dotfiles/angband/customize/* ~/.angband/Angband/customize/
-
-stow alacritty git vim wayland xorg zsh
