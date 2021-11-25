6901945951c24e833b185d3e8a4f4730d7ad8f78
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-11-25 00:54:40 -0700

    make angband.sh

---
+++ setup/angband.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mkdir ~/.angband/Angband/customize
+cp /usr/share/angband/angband/customize/* ~/.angband/Angband/customize/
+cp ~/.dotfiles/angband/customize/* ~/.angband/Angband/customize/
