c35230f7504ba68b4627083b83ae700c0309199a
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-11-27 18:02:53 -0700

    change background, remove some i3 config

--- setup/packages.sh
+++ setup/packages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo pacman -Syyuu --needed alacritty artix-backgrounds backlight-openrc bat bemenu brightnessctl clang cmake exa fd go go-tools imv links mpv noto-fonts noto-fonts-emoji npm nuget openrc-zsh-completions otf-fira-mono otf-fira-sans otf-font-awesome pulseaudio pulseaudio-alsa python-pip python-pynvim ripgrep stow sway swaylock grim texlive-core tmux tokei ttf-liberation typescript unzip wl-clipboard woff2-fira-code zsh-autosuggestions zsh-completions zsh-syntax-highlighting pulseaudio-alsa qemu aria2 cowsay yasm edk2-ovmf man-db pacman-contrib
+sudo pacman -Syyuu --needed alacritty backlight-openrc bat bemenu brightnessctl clang cmake exa fd go go-tools imv links mpv noto-fonts noto-fonts-emoji npm nuget openrc-zsh-completions otf-fira-mono otf-fira-sans otf-font-awesome pulseaudio pulseaudio-alsa python-pip python-pynvim ripgrep stow sway swaylock grim texlive-core tmux tokei ttf-liberation typescript unzip wl-clipboard woff2-fira-code zsh-autosuggestions zsh-completions zsh-syntax-highlighting pulseaudio-alsa qemu aria2 cowsay yasm edk2-ovmf man-db pacman-contrib
 
 sudo rc-update add backlight
 
