2fefd6d59e26ba8b1170886b58912dd9f9cb127b
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2021-11-29 19:37:36 -0700

    add gnome-keyring

--- setup/games.sh
+++ setup/games.sh
@@ -2,7 +2,7 @@
 
 sudo pacman -S rofi angband discord dosbox gnuchess gnugo retroarch rogue xorg-server xorg-xinit i3-gaps i3lock cataclysm-dda stone-soup dwarffortress
 
-paru -S minecraft-launcher
+paru -S minecraft-launcher gnome-keyring
 
 cargo install kingslayer
 cargo install gameoflife
